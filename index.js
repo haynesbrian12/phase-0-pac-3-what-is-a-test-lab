ef6e3cda750271ecbc3404c33766276a563bdc23
Brian Haynes <haynesbrian3@gmail.com>
2023-11-15 00:40:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const name = "Joe";
+const name = "Susan";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+describe("Height", (39) => {
+    it("is less than 40 and greater than 0", () => {
+      expect(height).toBeMoreThan(0)
+      expect(height).toBeLessThan(40);
+    });
+  });
